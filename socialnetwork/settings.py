10006e7a2dd66eb4879921850ab12015308b0ff4
Sashesh Singh <sashesh.sss@gmail.com>
2024-05-24 01:46:00 +0530

    feat: add docker files in the project

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -84,10 +84,14 @@ WSGI_APPLICATION = "socialnetwork.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": env.db(
-        "DATABASE_URL",
-        default="postgres:///socialnetwork",
-    ),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
+    }
 }
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
