6da5542be6cd390950fafe329baccac97bfac3b8
Sashesh Singh <sashesh.sss@gmail.com>
2024-05-23 18:04:34 +0530

    feat: added users sign up and login api

---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class User(AbstractUser):
+    USER_TYPE_CHOICES = [('admin', 'Admin'), ('vendor', 'Vendor'), ('user', 'User')]
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(unique=True)
+    user_type = models.CharField(_("User Type"), blank=True, max_length=255, choices=USER_TYPE_CHOICES)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
