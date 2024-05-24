9d1c891f5227d85795e9ae342c85381e4c8ba06a
Sashesh Singh <sashesh.sss@gmail.com>
2024-05-24 12:46:08 +0530

    feat: added api doc on default port

--- socialnetwork/urls.py
+++ socialnetwork/urls.py
@@ -28,5 +28,7 @@ urlpatterns = [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+    path("",         schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
